b0d4c6501892f9fcba9b6b3d5ec502d85c078b7c
Srijan Verma <bt23cse219@iiitn.ac.in>
2026-01-02 03:51:47 +0530

    Fixed cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from "dotenv";
+import colors from "colors";
 import bodyParser from "body-parser";
 import cors from "cors";
 
@@ -7,5 +8,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: true }))
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
